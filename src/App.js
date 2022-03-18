36c12de0b19fbd9b8c833f4d93b0fcfb3b276b80
NjGhtMaRe <lehongan15@gmail.com>
2022-03-18 09:51:06 +0700

    Add todolist

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, {useState, useEffect} from 'react';
 import queryString from 'query-string'
 import Pagination from "./components/PostList/Pagination";
 import PostFilters from "./components/PostList/PostFilters";
+import TodoList from "./components/TodoList/TodoList";
+import TodoForm from "./components/TodoList/TodoForm";
 function App() {
   const [postList, setPostList] = useState([])
   const [pagination, setPagination] = useState({})
@@ -13,6 +15,17 @@ function App() {
     _page: 1,
     title_like: ""
   })
+
+  const {
+    v1: uuidv1,
+    v4: uuidv4
+  } = require("uuid");
+
+  const [todoList, setTodoList] = useState([
+    {id: uuidv4(), title: "I'm Andy"},
+    {id: uuidv4(), title: "I'm Bin Bin"},
+    {id: uuidv4(), title: "I'm Tuk Tuk"}
+  ])
   useEffect(() => {
     async function fetchPostList() {
       try {
@@ -45,12 +58,31 @@ function App() {
       title_like: newFilters.searchTerm,
     })
   }
+
+  function handleTodoList(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id)
+    const newTodoList = [...todoList];
+    newTodoList.splice(index,1);
+    setTodoList(newTodoList)
+  }
+
+  function handleTodoSubmit(formValues) {
+    const newTodo = {
+      id: uuidv4(),
+      ...formValues
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList)
+  }
   return (
     <div className="App">
       <h1>Welcome to my class!</h1>
       <PostFilters onSubmit={handleFiltersChange} />
       <PostList posts={postList} />
       <Pagination pagination={pagination} onPageChange={handlePage} />
+      <TodoList todos={todoList} onTodoList={handleTodoList} />
+      <TodoForm onSubmit={handleTodoSubmit} />
     </div>
   );
 }
